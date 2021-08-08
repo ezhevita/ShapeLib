d3d5afef1ef8b86bb0ceb648da4fc4d49e6cf5af
Vitaliya Chumakova <vichuma9@gmail.com>
2021-08-09 00:09:19 +0300

    Add creation and insertion queries

--- query.sql
+++ query.sql
@@ -1,3 +1,38 @@
+CREATE TABLE categories
+(
+    id   int IDENTITY
+        CONSTRAINT categories_pk PRIMARY KEY NONCLUSTERED,
+    name varchar(100) NOT NULL
+)
+
+create table products
+(
+    id   int IDENTITY
+        CONSTRAINT products_pk PRIMARY KEY NONCLUSTERED,
+    name varchar(100) NOT NULL
+)
+
+create table categories_products
+(
+    product_id  int NOT NULL
+        CONSTRAINT categories_products_products_id_fk REFERENCES products,
+    category_id int NOT NULL
+        CONSTRAINT categories_products_categories_id_fk REFERENCES categories,
+    CONSTRAINT categories_products_pk
+        PRIMARY KEY NONCLUSTERED (product_id, category_id)
+)
+
+INSERT INTO products (name) VALUES ('test1'),
+                                   ('test2'),
+                                   ('test3');
+
+INSERT INTO categories (name) VALUES ('test_c1'),
+                                     ('test_c2');
+
+INSERT INTO categories_products (product_id, category_id) VALUES (2, 1),
+                                                                 (2, 2),
+                                                                 (3, 1);
+
 SELECT products.name   AS product_name,
        categories.name AS category_name
 FROM products
